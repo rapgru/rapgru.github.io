d51ce987cdf38dea4c9e849a6d171fdb77591c50
Raphael Gruber <raphael.gruber@gmail.com>
2020-06-26 22:18:54 +0200

    remove temp files

--- src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s - The Raphael Gruber Web Presence",
-  "siteUrl": "https://rapgru.com",
-  "version": "0.7.17",
-  "catchLinks": true
-}
